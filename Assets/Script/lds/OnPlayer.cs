7df999937b1324cb962af8a32d7ba0efa62a0af1
DaSom253 <idasom253@gmail.com>
2023-04-07 13:58:36 +0900

    Create
    맵과 플레이어(MainCamera) 생성, 현 obj는 구현이 어려운 단계이므로 Asset 활용하겠음
    
    OnPlayer Script 생성, 플레이어의 WASD 움직임을 담당함

---
+++ Assets/Script/lds/OnPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPlayer : MonoBehaviour
+{
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+    }
+}

3e48a1d17a74c20d130ec8a30d9a4ea13a27bf11
Yuhahak <dbwogkr1214@naver.com>
2023-05-05 13:05:43 +0900

    아이템 소유 여부 스크립트
    아이템 true/false를 관리하는 스크립트
    
    스크립트마다 instance를 남발하는 것은 최적화에 최악
    InstanceManager로 관리

---
+++ Assets/Script/InstanceManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InstanceManager : MonoBehaviour
+{
+    public static InstanceManager s; // 아이템 소유 여부를 판단할 스크립트
+    
+    private void Awake()
+    {
+        s = this;
+    }
+
+    public bool key1 = false;
+}
